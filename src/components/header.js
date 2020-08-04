c2e32722e9aba261a440872b147146b963f14f70
Moses Cosme <mosesmaxcosme@gmail.com>
2020-08-03 20:10:13 -0500

    cosmetic

--- src/components/header.js
+++ src/components/header.js
@@ -35,7 +35,7 @@ const Header = ({ siteTitle }) => (
                         if(!item.node.label.toLowerCase().includes('contact')){
                             return <li className="nav-item"><Link className="nav-link" to={`${item.node.label.toLowerCase()}`}>{item.node.label}</Link></li>
                         } else if(item.node.label.toLowerCase().includes('contact')){
-                            return <Link className="btn-teal btn rounded-pill px-4 ml-lg-4" to={`${item.node.label.toLowerCase()}`}>{item.node.label.toUpperCase()}<i className="fas fa-arrow-right ml-1"></i></Link>
+                            return <Link className="btn-teal btn rounded-pill px-4 ml-lg-4" to={`${item.node.label.toLowerCase().split(" ").shift()}`}>{item.node.label.toUpperCase()}<i className="fas fa-arrow-right ml-1"></i></Link>
                         }
                     })}
                 </ul>
