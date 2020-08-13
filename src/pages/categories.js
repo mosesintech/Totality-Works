57816333c51f02ecc66bb0893c2a7f745b6d26f8
Moses Cosme <mosesmaxcosme@gmail.com>
2020-08-13 18:11:35 -0500

    cosmetic

--- src/pages/categories.js
+++ src/pages/categories.js
@@ -22,7 +22,7 @@ const CategoriesPage = ({ data }) => {
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 144.54 17.34" preserveAspectRatio="none" fill="currentColor"><path d="M144.54,17.34H0V0H144.54ZM0,0S32.36,17.34,72.27,17.34,144.54,0,144.54,0"></path></svg>
         </div>
     </header>
-      <section class="bg-light py-10">
+      <section>
           <div class="container">
               <div class="row">
                 {cats.map(cat => {
