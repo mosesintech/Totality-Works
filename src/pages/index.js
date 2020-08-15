7578b698e33014b2922450c50709659bf52bd9dd
Moses Cosme <mosesmaxcosme@gmail.com>
2020-08-15 15:08:16 -0500

    commented out code to hit mvp

--- src/pages/index.js
+++ src/pages/index.js
@@ -109,9 +109,9 @@ const IndexPage = ({ data }) => {
                   )
                 })}
               </div>
-              <div class="text-center">
+              {/* <div class="text-center">
                 <Link class="btn btn-marketing btn-primary rounded-pill lift lift-sm" to={`${statCtaButtonUrl}`}>{statCtaButtonText}</Link>
-              </div>
+              </div> */}
           </div>
           <div class="svg-border-rounded text-white">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 144.54 17.34" preserveAspectRatio="none" fill="currentColor"><path d="M144.54,17.34H0V0H144.54ZM0,0S32.36,17.34,72.27,17.34,144.54,0,144.54,0"></path></svg>
